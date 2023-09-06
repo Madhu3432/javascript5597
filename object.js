d1bdae8704e0a2b89e91d407cb4a6a789e79fc5b
Madhu3432 <139035784+Madhu3432@users.noreply.github.com>
2023-09-06 20:23:34 +0530

    Add files via upload
    06/09/2023

---
+++ object.js
@@ -0,0 +1,32 @@
+const firstName = ["Madhu", "Krishna"];
+const surName = ["Gangireddy"];
+const NewArray = [];
+
+for (let i = 0; i < firstName.length; i++) {
+    // const fullName = `${firstName[i]} ${surName}`;
+    NewArray.push(`${firstName[i]} ${surName}`);
+}
+
+
+console.log(firstName);
+console.log(NewArray);
+
+//------------------------------------------------------------
+
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    getFullName() {
+        return this.firstName + ' ' + this.lastName;
+    }
+}
+
+const person1 = new Person('Madhu', 'Gangireddy');
+const person2 = new Person('Krishna', 'Gangireddy');
+
+console.log(person1.getFullName());
+console.log(person2.getFullName());
+
+
