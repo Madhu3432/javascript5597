7a1778526714ef9f513da87ae0723e9df596bbf7
Madhu3432 <139035784+Madhu3432@users.noreply.github.com>
2023-10-28 13:39:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript code to handle a button click event
+document.getElementById("myButton").addEventListener("click", function () {
+    let demoElement = document.getElementById("demo");
+    demoElement.innerHTML = "Button Clicked!";
+    demoElement.style.color = "green";
+});
