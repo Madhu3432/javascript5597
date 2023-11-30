1b4a2a370f4e38e22c9dceca7aa43668a5c2cd69
Madhu3432 <139035784+Madhu3432@users.noreply.github.com>
2023-11-30 12:42:45 +0530

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,5 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var currentDateElement = document.getElementById("currentDate");
-    var currentDate = new Date().toString();
-    currentDateElement.textContent = currentDate;
-});
+function addItem() {
+    var shoppingList = document.getElementById("shoppingList");
+    var newItem = document.createElement("li");
+    newItem.textContent = "New Item";
+    shoppingList.appendChild(newItem);
+}
+
+function removeItem() {
+    var shoppingList = document.getElementById("shoppingList");
+    var lastItem = shoppingList.lastElementChild;
+    if (lastItem) {
+        shoppingList.removeChild(lastItem);
+    }
+}
