1d97dd531673f08e59b1e42b43d7c88c94e61f8e
Madhu3432 <139035784+Madhu3432@users.noreply.github.com>
2023-11-30 12:41:04 +0530

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,16 +1,5 @@
-function createConcatPrint() {
-    var inputString = document.getElementById("inputString").value;
-    var resultElement = document.getElementById("result");
-
-    // Create and print the original string
-    var originalString = "Original String: " + inputString;
-
-    // Concatenate a new string
-    var concatenatedString = "Concatenated String: " + inputString + " - Hello World!";
-
-    // Access substring
-    var substring = "Substring: " + inputString.slice(0, 5);
-
-    // Display results
-    resultElement.innerHTML = originalString + "<br>" + concatenatedString + "<br>" + substring;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    var currentDateElement = document.getElementById("currentDate");
+    var currentDate = new Date().toString();
+    currentDateElement.textContent = currentDate;
+});
